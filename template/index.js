755d58aaf20ac051f95c048d67d99b12e00c11b6
Levan Beroshvili <lberoshvili9@gmail.com>
2019-04-30 03:31:28 +0400

    fixed minor changes

--- template/index.js
+++ template/index.js
@@ -1,5 +1,5 @@
 import Venom  from "venom-js";
-import AppComponent from "./app.component.js";
+import AppComponent from "./src/app.component.js";
 
 if (module.hot) {
   module.hot.accept()
--- template/webpack.common.js
+++ template/webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
-    app: './app.js'
+    app: './index.js'
   },
   output: {
     filename: '[name].bundle.js',
