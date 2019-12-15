f1cebca730e918a928e39cf4ebb8d3dc0fe3a858
Levan Beroshvili <lberoshvili9@gmail.com>
2019-12-15 23:22:48 +0100

    Removed extra super call from starting component

--- template/src/app.component.js
+++ template/src/app.component.js
@@ -1,10 +1,6 @@
 import {template, VenomComponent} from "venom-js";
 
 export default class AppComponent extends VenomComponent {
-  constructor() {
-    super();
-  }
-
   render() {
     return template`
       <div class="my-app">
